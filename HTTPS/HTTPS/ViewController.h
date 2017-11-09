f250551e71ef42ec72f573e206abd9dd9b004121
冀凯旋 <kaixuangogo@qq.com>
2017-11-09 17:01:47 +0800

    import code

---
+++ HTTPS/HTTPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPS
+//
+//  Created by Mobiyun on 2017/11/9.
+//  Copyright © 2017年 冀凯旋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTTPS/HTTPS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HTTPS
+//
+//  Created by Mobiyun on 2017/11/9.
+//  Copyright © 2017年 冀凯旋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
